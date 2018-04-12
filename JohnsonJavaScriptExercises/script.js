eb8beb8f2212976666cde1b5bb2f35261aff1bc4
kalebjohnson98 <kaleb.j.business@gmail.com>
2018-04-12 00:34:16 -0700

    Initial commit.

---
+++ JohnsonJavaScriptExercises/script.js
@@ -0,0 +1,41 @@
+//Exercise 1//
+
+var total=0;
+
+for (var num=1; num<=20; num++){
+  total= total + num;	
+}
+console.log("The Total is:" + total)
+}
+
+//Exercise 2//
+
+var q = "Do you want to play a game?"
+var question = prompt(q, "yes or no");
+if ("no") {
+    var outcome = stopiteration;
+}
+outcome;
+
+if ("yes") {
+    var w = "Enter a five letter word?"
+    var word = prompt(w)
+}
+outcome;
+
+alert(question);
+var response = ["no", "yes"];
+
+String; word;
+String; word2;
+
+alert(word);
+alert(word2)
+
+
+//Exercise 3//
+
+//******JavaScript is Confusing******//
+
+
+	
